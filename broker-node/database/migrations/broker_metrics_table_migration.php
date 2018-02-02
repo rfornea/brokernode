4398cd182d44d695d1a17ad2c53aab888f8674aa
Alex <smigaja@gmail.com>
2018-02-01 19:01:24 -0800

    Add migration for chunk events table.   Added class for updating chunk events such as successful attachment.

---
+++ broker-node/database/migrations/broker_metrics_table_migration.php
@@ -0,0 +1,43 @@
+
+
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChunkEvents extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+
+    public function up()
+    {
+        Schema::create('chunk_events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('hook_node_id');
+            $table->string('session_id');
+            $table->string('event_name');
+            $table->string('value');
+            $table->timestamps();
+        });
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('flights');
+    }
+}
+
+
+
+
+?>
+
